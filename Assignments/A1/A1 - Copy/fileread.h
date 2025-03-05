f700d28bf933b2f905dc54abc660ebb5d8030cf4
bro <percy.nguyen7@gmail.com>
2025-03-04 22:17:58 -0500

    remove a prev copy

--- Assignments/A1/A1 - Copy/fileread.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-char** read_file(int* size, char* fileName);
-void write_file(char** arr, int size);
-char** get_strings(int*);
--- Assignments/A1/A1 - Copy/ui.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include "wreplace.h"
- int confirmRes(int needConfirmation);
-void removeWordCaseSens(char** arr, int size, char* word);
